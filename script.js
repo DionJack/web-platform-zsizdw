bb1e5676059cba62845431a5cb7532cef2f38ceb
DionJack <dion.jackson0824@gmail.com>
2021-12-02 00:54:22 -0500

    several changes to function computer Play

--- script.js
+++ script.js
@@ -3,7 +3,18 @@
 let playerSelection = 'rock';
 let computerSelection = computerPlay();
 
-function computerPlay() {}
+function computerPlay() {
+  const arr =['rock', 'paper', 'scissors'];
+
+    //first element
+  console.log(arr[0]); // 'rock'
+
+  // second elment
+  console.log(arr[1]); // 'paper'
+
+  // third element
+  console.log(arr[2]); // 'scissors'
+}
 
 function playGame(playerSelection, computerSelection) {}
 

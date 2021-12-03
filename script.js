2b74a18df0631e6415b43520b7fba2c3ddb6cd18
DionJack <dion.jackson0824@gmail.com>
2021-12-02 19:23:35 -0500

    Wrote the retrurn function for  random  array

--- script.js
+++ script.js
@@ -4,28 +4,11 @@ let playerSelection = 'rock';
 let computerSelection = computerPlay();
 
 function computerPlay() {
-  const arr = ['rock', 'paper', 'scissors'];
-  const randomArr = Math.floor(Math.random() * 3);
-  if (randomArr === 0) {
-    return 'rock';
-  } else if (randomArr === 1) {
-    return 'paper';
-  } else if (randomArr === 3) {
-    return 'scissors';
-  }
-  {
-    console.log(computerPlay());
-  }
-
-  //first element
-  console.log(arr[0]); // 'rock'
-
-  // second elment
-  console.log(arr[1]); // 'paper'
-
-  // third element
-  console.log(arr[2]); // 'scissors'
+  let arr = ['rock', 'paper', 'scissors'];
+  let randomArr = Math.floor(Math.random() * 3);
+  return arr[randomArr];
 }
+console.log(computerSelection);
 
 function playGame(playerSelection, computerSelection) {}
 

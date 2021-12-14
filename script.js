2ea8b0be37dcca5685da6e4bd73b8e6e6aa9f122
DionJack <dion.jackson0824@gmail.com>
2021-12-13 23:34:52 -0500

    modified script.js

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
 
 // let playerSelection = prompt('Type your choice of rock, paper or scissors.');
-let playerSelection = 'rock';
-let computerSelection = computerPlay();
-
 function computerPlay() {
   let arr = ['rock', 'paper', 'scissors'];
+
   let randomArr = Math.floor(Math.random() * 3);
   return arr[randomArr];
 }
 
+let computerSelection = computerPlay();
+
 function playGame(playerSelection, computerSelection) {
 let result;
-  if (playerSelection === computerSelection)
-  console.log ('Tie, repeat to break the tie');
-  { else if (
+  if (playerSelection === computerSelection) {
+  result = 'Computer Choice ${Computer Selection} <br> Tie, repeat to break the tie';}  else if (
     (playerSelection === 'rock' && computerSelection === 'paper') ||
     (playerSelection === 'paper' && computerSelection === 'scissors') ||
     (playerSelection === 'scissors' && computerSelection === 'rock')
   ) { 
-    result =Computer Choice ${ ComputerSelection } 
-    console.log ( 'You Lose');    
-   { computerSelection} beats ${playerSelection};
+    result ='Computer Choice ${computerSelection <br> You Lose!$    
+   {computerSelection} beats ${playerSelection}.'
+  } else { 
+    result='Computer Choice ${computerSelection} <br> You Win!$
+    {playerSelection} beats ${computerSelection}.'
 
-   }else{ 
-    result= Computer Choice ${ ComputerSelection}
-    console.log ('You Win!')
-    {playerSelection} beats ${ computerSelection}
-
-    document.getElementById ('result').innerHTML= result ;
+    document.getElementById('result'.innerHTML = result;
    }
-   const buttons=document.querySelectorAll ('input');
+   const buttons = document.querySelectorAll('input');
 
    for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function () {
